03f963b3fcaecaf74418afce413520b58c090428
andriicherkas-unidatalab <cherkasandriy1122@gmail.com>
2026-06-11 16:42:28 +0300

    add lab events service

--- backend/app/application/services/lab_events.py
+++ backend/app/application/services/lab_events.py
@@ -1,7 +1,13 @@
 from typing import final
 
 from app.domain.interfaces.lab_events_service import ILabEventsService
+from app.domain.interfaces.lab_events_repository import ILabEventsRepository
 
 @final
 class LabEventsService(ILabEventsService):
+    def __init__(self, lab_events_repository: ILabEventsRepository):
+        self.lab_events_repository = lab_events_repository
+        
     
+    def get_by_admission_id(self, admission_id):
+        return self.lab_events_repository.get_by_admission_id(admission_id)
